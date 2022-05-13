96cb3ec1dc08c27571f8614a48a379fc98e2120f
evasimpetru28 <eva.simpetru@gmail.com>
2022-05-13 21:03:04 +0300

    clase noi

--- src/main/java/com/moodverse/appResource/Ambience.java
+++ src/main/java/com/moodverse/appResource/Ambience.java
@@ -1,4 +1,45 @@
 package com.moodverse.appResource;
 
 public class Ambience {
+
+    private int ambienceId;
+    private String sound;
+    private String ambienceName;
+
+    public Ambience(int ambienceId, String sound, String ambienceName) {
+        this.ambienceId = ambienceId;
+        this.sound = sound;
+        this.ambienceName = ambienceName;
+    }
+
+    public int getAmbienceId() {
+        return ambienceId;
+    }
+
+    public void setAmbienceId(int ambienceId) {
+        this.ambienceId = ambienceId;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public String getAmbienceName() {
+        return ambienceName;
+    }
+
+    public void setAmbienceName(String ambienceName) {
+        this.ambienceName = ambienceName;
+    }
+
+    @Override
+    public String toString() {
+        return "ambienceId " + ambienceId +
+                ", sound " + sound +
+                ", ambienceName " + ambienceName;
+    }
 }
